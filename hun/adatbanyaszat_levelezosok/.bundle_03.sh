ce1ce5d2b5158f083298407c2eeb7c3353d5aac4
Jabba Laci <jabba.laci@gmail.com>
2020-11-21 22:27:30 +0100

    L

--- hun/adatbanyaszat_levelezosok/.bundle_03.sh
+++ hun/adatbanyaszat_levelezosok/.bundle_03.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 NUM=03
-DIR="$NUM-sklearn"
+DIR="$NUM-numpy"
 ZIP="all_$NUM.zip"
 
 rm $DIR/$ZIP 2>/dev/null
---
+++ hun/adatbanyaszat_levelezosok/03-numpy/megoldasok/80_feladat.py
@@ -0,0 +1,23 @@
+# heights and positions are available as lists
+
+# Import numpy
+import numpy as np
+
+# Convert positions and heights to numpy arrays: np_positions, np_heights
+np_positions = np.array(positions)
+np_heights = np.array(heights)
+# print(np_positions.shape)
+# print(np_heights.shape)
+
+# Heights of the goalkeepers: gk_heights
+gk_heights = np_heights[np_positions == 'GK']
+
+# Heights of the other players: other_heights
+other_heights = np_heights[np_positions != 'GK']
+print(len(gk_heights), len(other_heights))
+
+# Print out the median height of goalkeepers. Replace 'None'
+print("Median height of goalkeepers: " + str(np.median(gk_heights)))
+
+# Print out the median height of other players. Replace 'None'
+print("Median height of other players: " + str(np.median(other_heights)))
