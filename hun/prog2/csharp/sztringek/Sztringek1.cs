aa86afcdd7c64c8e816270b30999f6e992bf2653
Jabba Laci <jabba.laci@gmail.com>
2020-11-25 12:01:18 +0100

    L

---
+++ hun/prog2/csharp/sztringek/Sztringek1.cs
@@ -0,0 +1,86 @@
+﻿using static System.Console;
+
+namespace Homework
+{
+    class Sztringek1
+    {
+        // A. donuts
+        // Bemenet: egy egész szám (a fánkok száma).
+        // Adjunk vissza egy sztringet a köv. formában: 'Fánkok száma: <count>',
+        // ahol <count> a bemenetként kapott érték.
+        // Viszont ha a fánkok száma 10 vagy több, akkor a tényleges szám helyett
+        // a 'sok' szót használjuk.
+        // Vagyis donuts(5) visszatérési értéke 'Fánkok száma: 5', míg
+        // donuts(23) visszatérési értéke 'Fánkok száma: sok'
+        private static string Donuts(int n)
+        {
+            return "";
+        }
+
+        // B. both_ends
+        // Egy adott s sztring esetén adjunk vissza egy olyan sztringet,
+        // mely az eredeti sztring első 2 és utolsó 2 karakteréből áll.
+        // Vagyis 'spring' esetén a visszatérési érték 'spng'.
+        // Ha az input sztring hossza 2-nél rövidebb, akkor egy üres
+        // sztringet adjunk vissza.
+        private static string BothEnds(string s)
+        {
+            return "";
+        }
+
+        // C. fix_start
+        // Egy adott s sztring esetén adjunk vissza egy olyan sztringet,
+        // melyben az első karakter összes előfordulása helyén egy '*'
+        // szerepel, kivéve a legelső pozíciót.
+        // Példa: 'babble' => 'ba**le'
+        // Feltételezhetjük, hogy a bemeneti sztring hossza legalább 1.
+        // Tipp: s.replace(stra, strb) egy olyan sztringet ad vissza,
+        // melyben az stra összes előfordulása ki van cserélve strb-re.
+        private static string FixStart(string s)
+        {
+            return "";
+        }
+
+        // D. MixUp
+        // Adott két bemeneti sztring, a és b. Adjunk vissza egyetlen sztringet,
+        // melyben a és b konkatenálva van úgy, hogy köztük egyetlen szóköz szerepel.
+        // Ezen túl cseréljük fel a két sztring első két karakterét az eredményben.
+        // Példa:
+        //   'mix', 'pod' -> 'pox mid'
+        //   'dog', 'dinner' -> 'dig donner'
+        // Feltételezhetjük, hogy a bemeneti sztringek hossza legalább 2.
+        private static string MixUp(string a, string b)
+        {
+            return "";
+        }
+
+        private static void Test(string got, string expected)
+        {
+            var prefix = (got == expected ? " OK " : "  X ");
+            WriteLine($"{prefix} got: {got}; expected: {expected}");
+        }
+
+        public static void Main(string[] args)
+        {
+            Test(Donuts(4), "Fánkok száma: 4");
+            Test(Donuts(9), "Fánkok száma: 9");
+            Test(Donuts(10), "Fánkok száma: sok");
+            Test(Donuts(99), "Fánkok száma: sok");
+            WriteLine("#");
+            Test(BothEnds("spring"), "spng");
+            Test(BothEnds("Hello"), "Helo");
+            Test(BothEnds("a"), "");
+            Test(BothEnds("xyz"), "xyyz");
+            WriteLine("#");
+            Test(FixStart("babble"), "ba**le");
+            Test(FixStart("aardvark"), "a*rdv*rk");
+            Test(FixStart("google"), "goo*le");
+            Test(FixStart("donut"), "donut");
+            WriteLine("#");
+            Test(MixUp("mix", "pod"), "pox mid");
+            Test(MixUp("dog", "dinner"), "dig donner");
+            Test(MixUp("gnash", "sport"), "spash gnort");
+            Test(MixUp("pezzy", "firm"), "fizzy perm");
+        }
+    }
+}
---
+++ hun/prog2/csharp/sztringek/Sztringek2.cs
@@ -0,0 +1,75 @@
+﻿using static System.Console;
+
+namespace Homework
+{
+    class Sztringek2
+    {
+        // E. verbing
+        // Ha az adott sztring hossza legalább 3, akkor
+        // a végéhez adjuk hozzá az 'ing' ragot.
+        // Ha 'ing'-re végződik, akkor ehelyett az 'ly'
+        // ragot tegyük hozzá.
+        // Ha a sztring hossza rövidebb 3 karakternél, akkor
+        // hagyjuk változatlanul.
+        // Adjuk vissza az eredménysztringet.
+        private static string Verbing(string s)
+        {
+            return "";
+        }
+
+        // F. not_bad
+        // Egy adott sztringben keressük meg a 'not' és
+        // 'bad' szavak előfordulási helyét. Ha a 'bad'
+        // a 'not' szót követi, akkor cseréljük ki az
+        // egész 'not'...'bad' részsztringet a 'good' szóra.
+        // Adjuk vissza az eredmény sztringet.
+        // Példa: 'This dinner is not that bad!' ->
+        //        This dinner is good!
+        private static string NotBad(string s)
+        {
+            return "";
+        }
+
+        // G. front_back
+        // Egy sztringet osszunk két részre, s a két részt nevezzük
+        // a sztring elejének és végének. Ha a sztring hossza páros, akkor
+        // a két rész hossza azonos. Ha a hossz páratlan, akkor az eleje
+        // legyen egy karakterrel hosszabb mint a vége.
+        // Például 'abcde' esetén a két rész: 'abc' és 'de'.
+        // Két adott sztring (a és b) esetén adjunk vissza egy sztringet, mely
+        // a következőképpen épül fel:
+        // a-eleje + b-eleje + a-vége + b-vége
+        // Például ha a = 'abcd' és b = 'xy', akkor az eredmény 'abxcdy' legyen.
+        private static string FrontBack(string a, string b)
+        {
+            return "";
+        }
+
+        private static void Test(string got, string expected)
+        {
+            var prefix = (got == expected ? " OK " : "  X ");
+            WriteLine($"{prefix} got: {got}; expected: {expected}");
+        }
+
+        public static void Main(string[] args)
+        {
+            WriteLine("verbing");
+            Test(Verbing("hail"), "hailing");
+            Test(Verbing("swiming"), "swimingly");
+            Test(Verbing("do"), "do");
+
+            WriteLine();
+            WriteLine("not_bad");
+            Test(NotBad("This movie is not so bad"), "This movie is good");
+            Test(NotBad("This dinner is not that bad!"), "This dinner is good!");
+            Test(NotBad("This tea is not hot"), "This tea is not hot");
+            Test(NotBad("It's bad yet not"), "It's bad yet not");
+
+            WriteLine();
+            WriteLine("front_back");
+            Test(FrontBack("abcd", "xy"), "abxcdy");
+            Test(FrontBack("abcde", "xyz"), "abcxydez");
+            Test(FrontBack("Kitten", "Donut"), "KitDontenut");
+        }
+    }
+}
