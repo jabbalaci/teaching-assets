25268a3da3f963baa7b0590675b54d98669efdbb
Jabba Laci <jabba.laci@gmail.com>
2022-09-30 16:44:35 +0200

    L

---
+++ hun/prog2/java/penteki_gyakorlatok/week_04/ranger/Main.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        List<Integer> numbers = PyUtils.range(20);
+        System.out.println(numbers);
+    }
+}
